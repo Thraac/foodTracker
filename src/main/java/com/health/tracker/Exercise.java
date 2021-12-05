f473a9091ba7780f4dbcff830f01ee63ceaae358
Nathan Wise <nathanofwise@gmail.com>
2021-12-05 14:49:08 -0700

    exercise

---
+++ src/main/java/com/health/tracker/Exercise.java
@@ -0,0 +1,81 @@
+package com.health.tracker;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="exercise")
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 64)
+    private Long user_id;
+
+    @Column(nullable = false, length = 64)
+    private String name;
+
+    @Column(nullable = false, length = 64)
+    private int weight;
+
+    @Column(nullable = false, length = 64)
+    private int sets;
+
+    @Column(nullable = false, length = 64)
+    private int reps;
+
+    // SETTERS GETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+}
